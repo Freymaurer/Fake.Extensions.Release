ded1898269a42af82c20faca79ac82528e10a5d0
Kevin F <Freymaurer@gmx.de>
2022-07-25 00:03:30 +0200

    Add build.fsproj

---
+++ build/ProjectInfo.fs
@@ -0,0 +1,35 @@
+﻿module ProjectInfo
+
+open Fake.Core
+
+let project = "Fake.Extensions.Release"
+
+let testProjects = 
+    [
+        // add relative paths (from project root) to your testprojects here
+        @"tests\Fake.Extensions.Release.Tests\Fake.Extensions.Release.Tests.fsproj"
+    ]
+
+let solutionFile  = $"{project}.sln"
+
+let configuration = "Release"
+
+let gitOwner = "Freymaurer"
+
+let gitHome = $"https://github.com/{gitOwner}"
+
+let projectRepo = $"https://github.com/{gitOwner}/{project}"
+
+let pkgDir = "pkg"
+
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+let stableVersion = SemVer.parse release.NugetVersion
+
+let stableVersionTag = (sprintf "%i.%i.%i" stableVersion.Major stableVersion.Minor stableVersion.Patch )
+
+let mutable prereleaseSuffix = ""
+
+let mutable prereleaseTag = ""
+
+let mutable isPrerelease = false
